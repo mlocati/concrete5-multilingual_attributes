08cfd24f9e10b2c01b5a816be9d79b9d5a482f7a
Michele Locati <michele@locati.it>
2014-06-16 11:07:18 +0200

    Add translation for "all languages"

--- models/attribute/types/multilingual_attribute/search_form.php
+++ models/attribute/types/multilingual_attribute/search_form.php
@@ -1,6 +1,6 @@
 <?php defined('C5_EXECUTE') or die('Access denied.');?>
 <select name="<?php echo $this->field('searchlocale'); ?>">
-	<option value="*">all languages</option>
+	<option value="*"><?php echo t('all languages'); ?></option>
 	<?php
 	foreach($locales as $localeID => $localeName) {
 		?><option value="<?php echo h($localeID) ?>"><?php echo tc('Only for language', 'Only for %s', $localeName); ?></option><?php
